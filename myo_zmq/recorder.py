d001307e5aee0680153916b5292904abfd7579da
egemenertugrul <egemenertugrul@yandex.com>
2023-01-20 14:11:56 +0300

    Update WIP

---
+++ myo_zmq/recorder.py
@@ -0,0 +1,135 @@
+import ctypes
+import multiprocessing
+from pathlib import Path
+
+from pyee import BaseEventEmitter
+import numpy as np
+import pandas as pd
+import os
+
+from typing import List
+
+
+class Recording:
+    data = []
+
+    def __init__(self, name, full_file_path, data=None):
+        if data is None:
+            data = []
+        self.name = name
+        self.full_file_path = full_file_path
+        self.data = data
+
+    def add(self, data_to_add):
+        self.data.append(data_to_add)
+
+    def get_data(self):
+        return np.array(self.data)
+
+    def get_data_count(self):
+        return len(self.data)
+
+    def get_name(self):
+        return self.name
+
+    def get_file_path(self):
+        return self.full_file_path
+
+
+class Recorder(BaseEventEmitter):
+    isRecording: bool = False
+    lastRecording: Recording = None
+    
+    def __init__(self, save_directory: str, desired_recording_length: int = -1, file_format: str = "csv"):
+        super(Recorder, self).__init__()
+        self.saveDirectory = save_directory
+        self.fileFormat = file_format
+        self.desiredRecordingLength = desired_recording_length
+
+    @staticmethod
+    def get_file_path(directory, file_name, file_format) -> str:
+        is_existing = os.path.exists(directory)
+        if not is_existing:
+            os.makedirs(directory)
+            print(f"A new directory is created at: {directory}")
+
+        i = 0
+        get_full_path = lambda: os.path.join(directory, f"{file_name}_{i}.{file_format}")
+        while os.path.exists(get_full_path()):
+            i += 1
+
+        return get_full_path()
+
+    @staticmethod
+    def get_recordings(directory, file_name, file_format) -> List[Recording]:
+        recordings = []
+        i = 0
+        get_full_path = lambda: os.path.join(directory, f"{file_name}_{i}.{file_format}")
+        while os.path.exists(get_full_path()):
+            path = get_full_path()
+            new_recording = Recording(path, data=pd.read_csv(path, header=None).as_matrix())
+            recordings.append(new_recording)
+            i += 1
+
+        return recordings
+
+    def begin_recording(self, file_name: str, force=False):
+        if self.isRecording:
+            if not force:
+                print("There is an ongoing recording. Use 'force' parameter to stop recording and start anew.")
+                return
+            self.end_recording(reset=True)
+
+        try:
+            full_file_path = self.get_file_path(self.saveDirectory, file_name, self.fileFormat)
+            self.lastRecording = Recording(file_name, full_file_path)
+        except Exception as e:
+            print("Error", e)
+            self.reset()
+            raise
+
+        self.isRecording = True
+        self.emit("begin_recording")
+
+    def add(self, data_to_add):
+        if not self.isRecording:
+            print("There is no recording in process to add data to.")
+            return
+
+        if self.desiredRecordingLength > 0:
+            if self.lastRecording.get_data_count() + 1 >= self.desiredRecordingLength:
+                print("Desired recording length reached")
+                self.save_recording()
+                return
+
+        self.lastRecording.add(data_to_add)
+
+    def end_recording(self, reset=False, silent=False):
+        if not self.isRecording:
+            print("There is no recording in process to stop.")
+            return
+
+        if not silent:
+            print("Ended recording: ", self.lastRecording.get_file_path())
+            self.emit("end_recording")
+
+        if reset:
+            self.reset()
+
+
+    def save_recording(self):
+        if self.isRecording:
+            self.end_recording(reset=False, silent=True)
+
+        if self.lastRecording is not None:
+            data = self.lastRecording.get_data()
+            df = pd.DataFrame(data)
+            file_path = self.lastRecording.get_file_path()
+            df.to_csv(Path(file_path), header=False)
+            print(f"Saved recording to file '{self.lastRecording.get_file_path()}', with duration of {len(data)} frames")
+            self.emit("save_recording")
+        self.reset()
+
+    def reset(self):
+        self.isRecording = False
+        self.lastRecording = None
--- subscriber.py
+++ subscriber.py
@@ -1,6 +1,12 @@
+from __future__ import annotations
+
 import asyncio
 import itertools
+import os
+import sys
 import time
+from typing import List
+
 import zmq.asyncio
 from dynamic_plot import Plot
 import multiprocessing
@@ -8,17 +14,24 @@ from multiprocessing import Process
 import numpy as np
 import json
 from myo_zmq.common import ZMQ_Topic
+from myo_zmq.recorder import Recording, Recorder
+import threading
 
 import matplotlib as mpl
 
+
 def clamp(n, smallest, largest): return max(smallest, min(n, largest))
 
-def remap(old_val, old_min, old_max, new_min, new_max): return (new_max - new_min)*(old_val - old_min) / (old_max - old_min) + new_min
 
-def colorFader(c1,c2,mix=0): #fade (linear interpolate) from color c1 (at mix=0) to c2 (mix=1)
-    c1=np.array(mpl.colors.to_rgb(c1))
-    c2=np.array(mpl.colors.to_rgb(c2))
-    return mpl.colors.to_hex((1-mix)*c1 + mix*c2)
+def remap(old_val, old_min, old_max, new_min, new_max): return (new_max - new_min) * (old_val - old_min) / (
+        old_max - old_min) + new_min
+
+
+def colorFader(c1, c2, mix=0):  # fade (linear interpolate) from color c1 (at mix=0) to c2 (mix=1)
+    c1 = np.array(mpl.colors.to_rgb(c1))
+    c2 = np.array(mpl.colors.to_rgb(c2))
+    return mpl.colors.to_hex((1 - mix) * c1 + mix * c2)
+
 
 def runGraphEMG(emg_queue):
     d_plot_2 = Plot(rowcol=(2, 4), max_display_capacity=500, ylim=(-150, 150))
@@ -44,7 +57,7 @@ def runGraphEMG(emg_queue):
     green = "#00ff00"
     blue = "#0000ff"
 
-    def process_data(epoch_time, emg_data):
+    def process_data(epoch_time, emg_data: multiprocessing.Queue):
         nonlocal last_means
 
         if listen_significant_changes:
@@ -91,7 +104,6 @@ def runGraphEMG(emg_queue):
         d_plot_2.add_to((2, 3), (epoch_time, emg_data[6]))
         d_plot_2.add_to((2, 4), (epoch_time, emg_data[7]))
 
-
     while True:
         while not emg_queue.empty():
             data = emg_queue.get()
@@ -109,7 +121,8 @@ def runGraphEMG(emg_queue):
                 process_data(epoch_time, emg_data)
         d_plot_2.flush()
 
-def runGraphIMU(imu_queue):
+
+def runGraphIMU(imu_queue: multiprocessing.Queue):
     take_mean = False
 
     d_plot = Plot(rowcol=(2, 3), max_display_capacity=500, style_args=
@@ -128,7 +141,6 @@ def runGraphIMU(imu_queue):
     d_plot.set_title((2, 1), "gyro x")
     d_plot.set_title((2, 2), "gyro y")
     d_plot.set_title((2, 3), "gyro z")
-
 
     while True:
         acc_vals = []
@@ -175,38 +187,198 @@ def runGraphIMU(imu_queue):
         d_plot.flush()
 
 
-async def Loop(imu_queue, emg_queue):
-    imu_t0 = time.time()
-    emg_t0 = time.time()
-    imu_count = 0
-    emg_count = 0
+# async def Loop(imu_queues: List[multiprocessing.Queue], emg_queues: List[multiprocessing.Queue]):
+#     imu_t0 = time.time()
+#     emg_t0 = time.time()
+#     imu_count = 0
+#     emg_count = 0
+#     while True:
+#         json_message = json.loads(socket.recv_json())
+#         topic = json_message["topic"]
+#         data = json_message["data"]
+#         epoch_time = json_message["time"]
+#
+#         data['time'] = epoch_time
+#
+#         if topic == ZMQ_Topic.IMU:
+#             for imu_queue in imu_queues:
+#                 imu_queue.put(data)
+#
+#             # print(packet)
+#             time_now = time.time()
+#             imu_count = imu_count + 1
+#             if time_now - imu_t0 > 1:
+#                 imu_t0 = time.time()
+#                 # print("IMU Hz: ", imu_count)
+#                 imu_count = 0
+#
+#         elif topic == ZMQ_Topic.EMG:
+#             for emg_queue in emg_queues:
+#                 emg_queue.put(data)
+#
+#             time_now = time.time()
+#             emg_count = emg_count + 1
+#             if time_now - emg_t0 > 1:
+#                 emg_t0 = time.time()
+#                 # print("EMG Hz: ", emg_count)
+#                 emg_count = 0
+
+
+class MainLoopThread(threading.Thread):
+    def __init__(self, imu_queues: List[multiprocessing.Queue], emg_queues: List[multiprocessing.Queue]):
+        super(MainLoopThread, self).__init__(daemon=True)
+        self.imu_queues = imu_queues
+        self.emg_queues = emg_queues
+        self.start()
+
+    def run(self):
+        imu_t0 = time.time()
+        emg_t0 = time.time()
+        imu_count = 0
+        emg_count = 0
+        while True:
+            json_message = json.loads(socket.recv_json())
+            topic = json_message["topic"]
+            data = json_message["data"]
+            epoch_time = json_message["time"]
+
+            data['time'] = epoch_time
+
+            if topic == ZMQ_Topic.IMU:
+                for imu_queue in self.imu_queues:
+                    imu_queue.put(data)
+
+                # print(packet)
+                time_now = time.time()
+                imu_count = imu_count + 1
+                if time_now - imu_t0 > 1:
+                    imu_t0 = time.time()
+                    print("IMU Hz: ", imu_count)
+                    imu_count = 0
+
+            elif topic == ZMQ_Topic.EMG:
+                for emg_queue in self.emg_queues:
+                    emg_queue.put(data)
+
+                time_now = time.time()
+                emg_count = emg_count + 1
+                if time_now - emg_t0 > 1:
+                    emg_t0 = time.time()
+                    print("EMG Hz: ", emg_count)
+                    emg_count = 0
+
+
+class KeyboardThread(threading.Thread):
+
+    def __init__(self, input_cbk=None, name='keyboard-input-thread'):
+        self.input_cbk = input_cbk
+        super(KeyboardThread, self).__init__(name=name, daemon=True)
+        self.start()
+
+    def run(self):
+        while True:
+            time.sleep(1)
+            self.input_cbk(input())
+
+
+def runRecorder(emg_queue: multiprocessing.Queue, keyboardInputQueue: multiprocessing.Queue):
+    # recorder = Recorder(save_directory=os.path.join(os.getcwd(), "recordings"), file_format='.csv')
+    recorder: Recorder = None
+
+    def process_data(epoch_time, emg_data):
+        if recorder:
+            if recorder.isRecording:
+                recorder.add(emg_data)
+
     while True:
-        json_message = json.loads(socket.recv_json())
-        topic = json_message["topic"]
-        data = json_message["data"]
-        epoch_time = json_message["time"]
+        if not keyboardInputQueue.empty():
+            inp_str = keyboardInputQueue.get()
+            if len(inp_str) > 0:
+                inp_str = inp_str.split(" ")
+                cmd = inp_str[0]
 
-        data['time'] = epoch_time
+                if cmd == "r" or cmd == "recorder":
+                    relative_recording_directory = "recordings"
+                    recording_len = -1
+                    file_format = "csv"
 
-        if topic == ZMQ_Topic.IMU:
-            imu_queue.put(data)
-            # print(packet)
-            time_now = time.time()
-            imu_count = imu_count + 1
-            if time_now-imu_t0 > 1:
-                imu_t0 = time.time()
-                print("IMU Hz: ", imu_count)
-                imu_count = 0
+                    if len(inp_str) > 1:
+                        relative_recording_directory = str(inp_str[1])
 
-        elif topic == ZMQ_Topic.EMG:
-            emg_queue.put(data)
+                        if len(inp_str) > 2:
+                            recording_len = int(inp_str[2])
 
-            time_now = time.time()
-            emg_count = emg_count + 1
-            if time_now - emg_t0 > 1:
-                emg_t0 = time.time()
-                print("EMG Hz: ", emg_count)
-                emg_count = 0
+                            if len(inp_str) > 3:
+                                file_format = str(inp_str[3])
+
+                    save_directory = os.path.join(os.getcwd(), relative_recording_directory)
+                    recorder = Recorder(save_directory=save_directory,
+                                        desired_recording_length=recording_len,
+                                        file_format=file_format)
+
+                    print(
+                        f"Created recorder with save_directory: {save_directory}, desired_recording_length: {recording_len}, file_format: {file_format}")
+                    print(f"To begin the recording,\n (b)egin\n\t<gesture_name (default: 'test_gesture', str)>")
+
+                    def on_begin_recording():
+                        print(f"To end and save the recording,\n (s)ave")
+                        print(f"To end the recording,\n (e)nd")
+
+                    def on_end_recording():
+                        print(f"To save the recording,\n (s)ave")
+                        print(
+                            f"To discard the current one and begin a new recording,\n (b)egin\n\t<gesture_name ("
+                            f"default: 'test_gesture', str)>")
+
+                    def on_save_recording():
+                        print(f"To begin the recording,\n (b)egin\n\t<gesture_name (default: 'test_gesture', str)>")
+
+                    recorder.on('begin_recording', on_begin_recording)
+                    recorder.on('end_recording', on_end_recording)
+                    recorder.on('save_recording', on_save_recording)
+
+                if cmd == "b" or cmd == "begin":
+                    gesture_name = "gesture_name"
+                    if len(inp_str) > 1:
+                        gesture_name = str(inp_str[1])
+                    if recorder:
+                        recorder.begin_recording(gesture_name)
+
+                if cmd == "e" or cmd == "end":
+                    if recorder:
+                        recorder.end_recording()
+
+                if cmd == "s" or cmd == "save":
+                    if recorder:
+                        recorder.save_recording()
+
+                # print(f"{inp}")
+
+        while not emg_queue.empty():
+            data = emg_queue.get()
+
+            shape = np.array(data['emg']).shape
+            epoch_time = float(data['time'])
+
+            if shape[0] == 2:
+                for i in range(len(data['emg'])):
+                    emg_data = data['emg'][i]
+                    emg_data = list(map(float, emg_data))
+                    process_data(epoch_time, emg_data)
+            else:
+                emg_data = list(map(float, data['emg']))
+                process_data(epoch_time, emg_data)
+
+
+def true_false_prompt(text: str, expected_vals: dict | None = None) -> bool:
+    if text is None:
+        print("Prompt text must be provided.")
+    if expected_vals is None:
+        expected_vals = {'y': True, 'n': False}
+    while True:
+        prompt_res = str(input(text + '\n')).lower()
+        if prompt_res in expected_vals.keys():
+            return expected_vals[prompt_res]
 
 
 if __name__ == '__main__':
@@ -218,18 +390,54 @@ if __name__ == '__main__':
     socket.connect(url)
     socket.setsockopt(zmq.SUBSCRIBE, b'')
     socket.setsockopt(zmq.RCVHWM, 1000)
-
     ctx = zmq.asyncio.Context()
 
-    imu_queue = multiprocessing.Queue()
-    emg_queue = multiprocessing.Queue()
+    imuQueues = []
+    emgQueues = []
+    all_processes = []
 
-    p_IMU = Process(target=runGraphIMU, args=(imu_queue, ))
-    p_IMU.start()
+    isPlotIMU = true_false_prompt("Would you like to PLOT IMU data? (y/n)")
 
-    p_EMG = Process(target=runGraphEMG, args=(emg_queue, ))
-    p_EMG.start()
+    if isPlotIMU:
+        imuQueue = multiprocessing.Queue()
+        imuQueues.append(imuQueue)
+        p_IMU = Process(target=runGraphIMU, args=(imuQueue,), daemon=True)
+        all_processes.append(p_IMU)
+        p_IMU.start()
 
-    asyncio.run(Loop(imu_queue, emg_queue))
-    p_IMU.join()
-    p_EMG.join()
+    isPlotEMG = true_false_prompt("Would you like to PLOT EMG data? (y/n)")
+
+    if isPlotEMG:
+        emgQueue = multiprocessing.Queue()
+        emgQueues.append(emgQueue)
+        p_EMG = Process(target=runGraphEMG, args=(emgQueue,), daemon=True)
+        all_processes.append(p_EMG)
+        p_EMG.start()
+
+    isRecordEMG = true_false_prompt("Would you like to RECORD EMG data? (y/n)")
+
+    if isRecordEMG:
+        emgQueueToRecord = multiprocessing.Queue()
+        emgQueues.append(emgQueueToRecord)
+
+        keyboardInputQueue = multiprocessing.Queue()
+        p_Recorder = Process(target=runRecorder, args=(emgQueueToRecord, keyboardInputQueue), daemon=True)
+        all_processes.append(p_Recorder)
+        p_Recorder.start()
+
+
+        def keyboard_callback(inp):
+            inp = str(inp).lower()
+            keyboardInputQueue.put(inp)
+
+
+        kthread = KeyboardThread(keyboard_callback)
+        print(
+            "(r)ecord \n\t<relative_recording_directory('default: recordings', str)>\n\t<recording_len ('default: -1', "
+            "int)>\n\t<file_format ('default: csv', str)>")
+
+    mainLoopThread = MainLoopThread(imuQueues, emgQueues)
+    # asyncio.run(Loop([imuQueue], [emgQueue, emgQueueToRecord]))
+
+    for process in all_processes:
+        process.join()
